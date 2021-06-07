9bc364e192b6448632ee8e809c85b68e9f5a9cf8
jaime <jaime_reis18@outlook.pt>
2021-06-07 22:37:40 +0000

    Moved project

---
+++ YugiohGanda.DataAccess/Models/Monster.cs
@@ -0,0 +1,17 @@
+﻿using YugiohGanda.Core.Common;
+
+namespace YugiohGanda.Core.Models
+{
+    public class Monster : Card
+    {
+        public int AttackPoints { get; set; }
+
+        public int DefensePoints { get; set; }
+
+        public int Level { get; set; }
+
+        public MonsterAttribute MonsterAttribute { get; set; }
+        
+        public MonsterType MonsterType { get; set; }
+    }
+}
